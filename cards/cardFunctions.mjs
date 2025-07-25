68e1df8fcab75fe1d09df621acc88b8e46e83a41
joshjones49 <joshjones200285@gmail.com>
2025-07-25 15:30:21 -0400

    Completed CRUD functionality for cards

--- cards/cardFunctions.mjs
+++ cards/cardFunctions.mjs
@@ -25,3 +25,32 @@ export const fuzzySearch = async (req, res) => {
         res.status(500).json({error: error.message});
     }
 }
+
+export const createCard = async (req, res) => {
+    const { question, answer, category } = req.body;
+
+    if (!question || !answer || !category) {
+        return res.status(400).json({ error: "Please complete all fields" });
+    }
+
+    try {
+        const result = await pool.query(
+            "INSERT INTO cards (question, answer, category) VALUES ($1, $2, $3) RETURNING *",
+            [question, answer, category]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const deleteCard = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const result = await pool.query("DELETE FROM cards WHERE id = $1", [id]);
+        res.status(200).json(result.rows);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
--- cards/cards.mjs
+++ cards/cards.mjs
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { getAllCards, fuzzySearch } from './cardFunctions.mjs'
+import { getAllCards, fuzzySearch, createCard, deleteCard } from './cardFunctions.mjs'
 
 const router = express.Router()
 
@@ -11,5 +11,13 @@ router.get('/', async (req, res) => {
 router.get('/:search', async (req, res) => {
         await fuzzySearch(req, res)
 });
+
+router.post('/', async (req, res) => {
+    await createCard(req, res)
+})
+
+router.delete('/delete/:id', async (req, res) => {
+    await deleteCard(req, res)
+})
 
 export default router
